ae786069411a117713dca22073164b353badd53d
saemii-24 <hims2419@gmail.com>
2024-07-09 23:27:27 +0900

    Feat: format으로 작성

---
+++ string.py
@@ -0,0 +1,13 @@
+course = 'Python for Beginners'
+
+print(course[0])
+print(course[-2])
+print(course[0:3])
+print(course[0:])
+print(course[1:])
+print(course[:5])
+print(course[:])
+
+name = 'yooni'
+# 1번 인덱스부터 -1자리를 제외하고 oon이 출력됨
+print(name[1:-1])
