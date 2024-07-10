46da4e87f790196cd2bb366285b62eeacaff0d32
saemii-24 <hims2419@gmail.com>
2024-07-10 22:49:50 +0900

    Feat: python number method

---
+++ number.py
@@ -0,0 +1,20 @@
+print(10/3)
+print(10//3) #정수 나눗셈 (소수점 이하 버리고 정수부분만 반환함)
+print(10%3)
+print(10**3) # 10^3
+
+x = 10
+x = x + 3
+print(x)
+
+x += 3
+print(x)
+
+y = 2.9
+print(round(y)) #반올림
+print(abs(-2.9)) #절대값
+
+#수학 모듈 불러오기
+import math
+print(math.ceil(2.9))#올림
+print(math.floor(2.9))#버림
