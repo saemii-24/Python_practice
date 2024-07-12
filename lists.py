6388c8c517a9a13cd8aa7772d7465e3d6f9707d9
saemii-24 <hims2419@gmail.com>
2024-07-12 23:34:47 +0900

    Feat: list 관련 메소드

--- lists.py
+++ lists.py
@@ -17,15 +17,42 @@ for row in matrix:
     
 numbers = [5,1,2,7,4]
 # numbers.append(20)
+
+# 인덱스 0번에 10 삽입
 numbers.insert(0,10)
+
+# 리스트 첫번째 4 제거
 numbers.remove(4)
-print(numbers)
 
+# 리스트 마지막 요소 제거, 그 값 반환
 numbers.pop()
-print(numbers)
 
+# 리스트에 3이 있는 지 확인 True / False
+3 in numbers
+
+#5개 갯수 세어 반환
+numbers.count(5)
+
+# 값 5의 첫번째 인덱스 반환
 numbers.index(5)
-print(numbers)
 
+# 리스트 오름차순 정렬
+numbers.sort()
+
+# 리스트 순서 뒤집기
+numbers.reverse()
+
+#복사본 만들어 numbers2에 저장
+numbers2 = numbers.copy()
+
+# 리스트 모든 요소 제거
 numbers.clear()
-print(numbers)
+
+# 중복값 지우기
+numbers= [2,2,4,6,3,4,6,1]
+unique = []
+
+for number in numbers:
+  if number not in unique:
+    unique.append(number)
+print(unique)
