54309990be00baa5aebb348c8230ec9960e2a60a
saemii-24 <hims2419@gmail.com>
2024-07-15 20:42:48 +0900

    Feat: module

---
+++ module.py
@@ -0,0 +1,12 @@
+import converters
+from converters import kg_to_lbs
+
+kg_to_lbs(1000)
+
+print(converters.kg_to_lbs(70))
+
+# import converters로 불러오기
+# converters 모듈 전체를 가져와서 사용하는 것으로, 이 경우 converters.kg_to_lbs(70) 형태로 사용한다.
+
+# from convertes import kg_to_lbs
+#kg_to_lbs 함수만 가져와서 사용한다.
